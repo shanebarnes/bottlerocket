60ac94a6af99d496789699fb5811ed5e54c902a7
shanebarnes <shanebarnes@users.noreply.github.com>
2016-03-01 21:30:23 -0500

    system_types: initial commit

---
+++ inc/system_types.h
@@ -0,0 +1,16 @@
+/**
+ * @file   system_types.h
+ * @author Shane Barnes
+ * @date   01 Mar 2016
+ * @brief  Standard and custom system type definitions.
+ */
+
+#ifndef _SYSTEM_TYPES_H_
+#define _SYSTEM_TYPES_H_
+
+#include <inttypes.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#endif // _SYSTEM_TYPES_H_
