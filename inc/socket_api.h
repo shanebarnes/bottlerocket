ab2875cce93ddeac23f60c3f51eb69bc962c2cd5
shanebarnes <shanebarnes@users.noreply.github.com>
2016-03-04 18:52:29 -0600

    socket_api: replaced file descriptor with socket_instance struct

--- inc/socket_api.h
+++ inc/socket_api.h
@@ -8,6 +8,7 @@
 #ifndef _SOCKET_API_H_
 #define _SOCKET_API_H_
 
+#include "socket.h"
 #include "system_types.h"
 
 struct socket_api
@@ -15,87 +16,90 @@ struct socket_api
     /**
      * @brief Open a socket.
      *
-     * @param[in] sockfd An unused socket file descriptor.
+     * @param[in,out] instance A pointer to a socket instance structure.
      *
      * @return True on success.
      */
-    bool (*socket_api_open)(const int32_t sockfd);
+    bool (*socket_api_open)(struct socket_instance * const instance);
 
     /**
      * @brief Close a socket.
      *
-     * @param[in] sockfd An open socket file descriptor.
+     * @param[in,out] instance A pointer to a socket instance structure.
      *
      * @return True on success.
      */
-    bool (*socket_api_close)(const int32_t sockfd);
+    bool (*socket_api_close)(struct socket_instance * const instance);
 
     /**
      * @brief Assign an address to a socket.
      *
-     * @param[in] sockfd An open socket file descriptor.
+     * @param[in,out] instance A pointer to a socket instance structure.
      *
      * @return True on success.
      */
-    bool (*socket_api_bind)(const int32_t sockfd);
+    bool (*socket_api_bind)(struct socket_instance * const instance);
 
     /**
      * @brief Listen for connections on an open socket.
      *
-     * @param[in] sockfd  An open socket file descriptor.
-     * @param[in] backlog The maximum length of the pending connection queue
-     *                    for the socket.
+     * @param[in,out] instance A pointer to a socket instance structure.
+     * @param[in]     backlog  The maximum length of the pending connection
+     *                         queue for the socket.
      *
      * @return True on success.
      */
-    bool (*socket_api_listen)(const int32_t sockfd, const int32_t backlog);
+    bool (*socket_api_listen)(struct socket_instance * const instance,
+                              const int32_t backlog);
 
     /**
      * @brief Accept a connection on a listener socket.
      *
-     * @param[in] sockfd    An open listener socket file descriptor.
-     * @param[in] timeoutms The accept timeout in milliseconds.
+     * @param[in,out] instance  A pointer to a socket instance structure.
+     * @param[in]     timeoutms The accept timeout in milliseconds.
      *
      * @return A non-negative integer that is the accepted socket file
      *         descriptor (-1 on error).
      */
-    int32_t (*socket_api_accept)(const int32_t sockfd, const int32_t timeoutms);
+    int32_t (*socket_api_accept)(struct socket_instance * const instance,
+                                 const int32_t timeoutms);
 
     /**
      * @brief Initiate a connection on a socket.
      *
-     * @param[in] sockfd    An open socket file descriptor.
-     * @param[in] timeoutms The connect timeout in milliseconds.
+     * @param[in,out] instance  A pointer to a socket instance structure.
+     * @param[in]     timeoutms The connect timeout in milliseconds.
      *
      * @return True on success.
      */
-    bool (*socket_api_connect)(const int32_t sockfd, const int32_t timeoutms);
+    bool (*socket_api_connect)(struct socket_instance * const instance,
+                               const int32_t timeoutms);
 
     /**
      * @brief Receive data from a socket.
      *
-     * @param[in]     sockfd An open socket file descriptor.
-     * @param[in,out] buf    A pointer to a buffer to store data received from
-     *                       the socket.
-     * @param[in]     len    The maximum size of the receive buffer in bytes.
+     * @param[in,out] instance A pointer to a socket instance structure.
+     * @param[in,out] buf      A pointer to a buffer to store data received from
+     *                         the socket.
+     * @param[in]     len      The maximum size of the receive buffer in bytes.
      *
      * @return The number of bytes received from the socket (-1 on error).
      */
-    int32_t (*socket_api_recv)(const int32_t sockfd,
+    int32_t (*socket_api_recv)(struct socket_instance * const instance,
                                void * const buf,
                                const uint32_t len);
 
     /**
      * @brief Send data to a socket.
      *
-     * @param[in] sockfd An open socket file descriptor.
-     * @param[in] buf    A pointer to a buffer containing data to be sent to
-     *                   the socket.
-     * @param[in] len    The maximum number of bytes in the send buffer.
+     * @param[in,out] instance A pointer to a socket instance structure.
+     * @param[in]     buf      A pointer to a buffer containing data to be sent
+     *                         to the socket.
+     * @param[in]     len      The maximum number of bytes in the send buffer.
      *
      * @return The number of bytes sent to the socket (-1 on error).
      */
-    int32_t (*socket_api_send)(const int32_t sockfd,
+    int32_t (*socket_api_send)(struct socket_instance * const instance,
                                void * const buf,
                                const uint32_t len);
 };
