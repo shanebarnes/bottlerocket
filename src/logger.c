a49eacd800a61709f38789bb3f452341ea98e626
shanebarnes <shanebarnes@users.noreply.github.com>
2016-03-09 20:05:28 -0500

    logger: use manip_date.h API to get formatted wall clock time

--- src/logger.c
+++ src/logger.c
@@ -6,11 +6,10 @@
  */
 
 #include "logger.h"
+#include "manip_date.h"
 
 #include <stdarg.h>
 #include <stdio.h>
-#include <sys/time.h>
-#include <time.h>
 
 static enum logger_level static_level = LOGGER_LEVEL_ALL;
 
@@ -80,9 +79,7 @@ void logger_printf(const enum logger_level level, const char *format, ...)
     int32_t error = 0;
     char msgbuf[1024], timebuf[512];
     va_list args;
-    struct timeval tv;
-    struct tm *tm = NULL;
-    time_t time;
+    uint64_t sec = 0, nsec = 0;
 
     if ((static_level != LOGGER_LEVEL_OFF) &&
         ((level >= static_level) || (static_level == LOGGER_LEVEL_ALL)))
@@ -93,14 +90,17 @@ void logger_printf(const enum logger_level level, const char *format, ...)
 
         if (error >= 0)
         {
-            error = gettimeofday(&tv, NULL);
-            time = tv.tv_sec;
-            tm = localtime(&time);
-            strftime(timebuf, sizeof(timebuf), "%Y-%m-%dT%H:%M:%S", tm);
+            manip_date_time(DATE_CLOCK_REALTIME, &sec, &nsec);
+            manip_date_time_format(sec,
+                                   UNIT_TIME_SEC,
+                                   "%Y-%m-%dT%H:%M:%S",
+                                   timebuf,
+                                   sizeof(timebuf));
+
             fprintf(stdout,
                     "%s.%06u [%-5s]: %s",
                     timebuf,
-                    (uint32_t)tv.tv_usec,
+                    (uint32_t)nsec / 1000,
                     logger_get_level_string(level),
                     msgbuf);
         }
