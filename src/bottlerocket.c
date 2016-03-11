a252315e9a6b275c9eb46e3b119c9659bc3b33fc
shanebarnes <shanebarnes@users.noreply.github.com>
2016-03-10 21:19:57 -0500

    bottlerocket: added sample thread worker function

--- src/bottlerocket.c
+++ src/bottlerocket.c
@@ -6,6 +6,7 @@
  */
 
 #include "logger.h"
+#include "pthreads_instance.h"
 #include "system_types.h"
 
 #include <signal.h>
@@ -28,21 +29,64 @@ void signal_handler(int signum)
     {
         case SIGHUP:
             logger_printf(LOGGER_LEVEL_INFO, "Caught SIGHUP\n");
+            break;
         case SIGINT:
             logger_printf(LOGGER_LEVEL_INFO, "Caught SIGINT\n");
+            break;
         case SIGKILL:
             logger_printf(LOGGER_LEVEL_INFO, "Caught SIGKILL\n");
+            break;
+        case SIGQUIT:
+            logger_printf(LOGGER_LEVEL_INFO, "Caught SIGQUIT\n");
+            break;
         case SIGSEGV:
             logger_printf(LOGGER_LEVEL_INFO, "Caught SIGSEGV\n");
+            break;
         case SIGSTOP:
             logger_printf(LOGGER_LEVEL_INFO, "Caught SIGSTOP\n");
+            break;
         case SIGTERM:
             logger_printf(LOGGER_LEVEL_INFO, "Caught SIGTERM\n");
+            break;
         default:
-            shutdown = true;
+            logger_printf(LOGGER_LEVEL_INFO, "Caught signal %d\n", signum);
     }
 
-    exit(signum);
+    shutdown = true;
+    //exit(signum);
+}
+
+/**
+ * @brief Thread worker function.
+ *
+ * @param[in] arg Thread input argument.
+ *
+ * @return NULL.
+ */
+void *thread_function(void * arg)
+{
+    struct pthreads_instance *instance = (struct pthreads_instance *)arg;
+
+    logger_printf(LOGGER_LEVEL_TRACE, "%s: Starting thread\n", __FUNCTION__);
+
+    if (instance != NULL)
+    {
+        while (instance->shutdown == false)
+        {
+            logger_printf(LOGGER_LEVEL_TRACE, "Thread running\n");
+            usleep(1000 * 1000);
+        }
+    }
+    else
+    {
+        logger_printf(LOGGER_LEVEL_ERROR,
+                      "%s: thread instance is invalid\n",
+                      __FUNCTION__);
+    }
+
+    logger_printf(LOGGER_LEVEL_TRACE, "%s: Stopping thread\n", __FUNCTION__);
+
+    return NULL;
 }
 
 /**
@@ -57,6 +101,7 @@ void signal_handler(int signum)
 int32_t main(int argc, char **argv)
 {
     int32_t retval = EXIT_SUCCESS;
+    struct pthreads_instance thread;
 
     logger_set_level(LOGGER_LEVEL_INFO);
 
@@ -64,13 +109,25 @@ int32_t main(int argc, char **argv)
     signal(SIGHUP,  signal_handler);
     signal(SIGINT,  signal_handler);
     signal(SIGKILL, signal_handler);
+    signal(SIGQUIT, signal_handler);
     signal(SIGSEGV, signal_handler);
     signal(SIGSTOP, signal_handler);
+    signal(SIGTERM, signal_handler);
 
     if (argc > 0 && argv)
     {
+        thread.function = thread_function;
+        thread.argument = &thread;
+        thread.shutdown = false;
 
+        pthreads_instance_create(&thread);
+        pthreads_instance_start(&thread);
     }
+
+    pause();
+
+    pthreads_instance_stop(&thread);
+    pthreads_instance_destroy(&thread);
 
     shutdown = true;
 
