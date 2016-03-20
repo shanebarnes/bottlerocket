fe9c5f245f1f7480ceaa8e1caa4ba7c95cfc719a
shanebarnes <shanebarnes@users.noreply.github.com>
2016-03-19 22:34:05 -0400

    io_event_poll: updated comments

--- inc/io_event_poll.h
+++ inc/io_event_poll.h
@@ -8,21 +8,21 @@
 #ifndef _IO_EVENT_POLL_H_
 #define _IO_EVENT_POLL_H_
 
-#include "io_event_api.h"
+#include "io_event_instance.h"
 #include "system_types.h"
 
 /**
- * @see io_event_api.h for interface comments.
+ * @see ieo_create() for interface comments.
  */
 bool io_event_poll_create(struct io_event_instance * const instance);
 
 /**
- * @see io_event_api.h for interface comments.
+ * @see ieo_destroy() for interface comments.
  */
 bool io_event_poll_destroy(struct io_event_instance * const instance);
 
 /**
- * @see io_event_api.h for interface comments.
+ * @see ieo_poll() for interface comments.
  */
 bool io_event_poll_poll(struct io_event_instance * const instance);
 
