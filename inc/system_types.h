0b9a86e7da3ad3aeb44436c0c86846eabbf4b387
shanebarnes <shanebarnes@users.noreply.github.com>
2016-03-25 14:09:58 -0400

    system_types: added copyright tag

--- inc/system_types.h
+++ inc/system_types.h
@@ -1,8 +1,10 @@
 /**
- * @file   system_types.h
- * @author Shane Barnes
- * @date   01 Mar 2016
- * @brief  Standard and custom system type definitions.
+ * @file      system_types.h
+ * @brief     Standard and custom system type definitions.
+ * @author    Shane Barnes
+ * @date      01 Mar 2016
+ * @copyright Copyright 2016 Shane Barnes. All rights reserved.
+ *            This project is released under the MIT license.
  */
 
 #ifndef _SYSTEM_TYPES_H_
