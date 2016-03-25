d90dcf2dfb1a930bb685057afc10835ae04e0e24
shanebarnes <shanebarnes@users.noreply.github.com>
2016-03-25 14:09:57 -0400

    output_if_std: added copyright tag

--- inc/output_if_std.h
+++ inc/output_if_std.h
@@ -1,8 +1,10 @@
 /**
- * @file   output_if_std.h
- * @author Shane Barnes
- * @date   14 Mar 2016
- * @brief  Standard output stream interface.
+ * @file      output_if_std.h
+ * @brief     Standard output stream interface.
+ * @author    Shane Barnes
+ * @date      14 Mar 2016
+ * @copyright Copyright 2016 Shane Barnes. All rights reserved.
+ *            This project is released under the MIT license.
  */
 
 #ifndef _OUTPUT_IF_STD_H_
