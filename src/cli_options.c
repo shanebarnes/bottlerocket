47f8a35adde97e249b89f94be26d0fc7934f5d6b
shanebarnes <shanebarnes@users.noreply.github.com>
2016-03-25 14:09:58 -0400

    cli_options: added copyright tag

--- src/cli_options.c
+++ src/cli_options.c
@@ -1,8 +1,10 @@
 /**
- * @file   cli_options.c
- * @author Shane Barnes
- * @date   12 Mar 2016
- * @brief  Command-line interface options implementation.
+ * @file      cli_options.c
+ * @brief     Command-line interface options implementation.
+ * @author    Shane Barnes
+ * @date      12 Mar 2016
+ * @copyright Copyright 2016 Shane Barnes. All rights reserved.
+ *            This project is released under the MIT license.
  */
 
 #include "cli_options.h"
