7afdac8c71a1e72ab530622f26420276787fb88b
shanebarnes <shanebarnes@users.noreply.github.com>
2016-04-29 21:34:46 -0400

    form_obj: added idle and spinner functions

--- inc/form_obj.h
+++ inc/form_obj.h
@@ -51,6 +51,28 @@ struct formobj_ops
     int32_t (*form_foot)(struct formobj * const obj);
 };
 
+/**
+ * @brief Create and format a data idle message and store it in the format
+ *        object destination buffer.
+ *
+ * @param[in,out] obj A pointer to a format object.
+ *
+ * @return The number of formatted bytes in the format object destination
+ *         buffer (-1 on error).
+ */
+int32_t formobj_idle(struct formobj * const obj);
+
+/**
+ * @brief Get a spinner character. The character returned will change every time
+ *        the function is called.
+ *
+ * @param[in] obj A pointer to a format object.
+ *
+ * @return The latest spinner character for a format object (null character on
+ *         error).
+ */
+char formobj_spin(struct formobj * const obj);
+
 struct formobj
 {
     struct formobj_ops  ops;
@@ -59,6 +81,7 @@ struct formobj
                        *dstbuf;
     int32_t             srclen,
                         dstlen;
+    uint16_t            spincount;
     uint64_t            timeoutusec;
 };
 
