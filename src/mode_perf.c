29b3cb7bb0211c2a6cb0fa6b19a05b43539100c4
shanebarnes <shanebarnes@users.noreply.github.com>
2016-07-01 08:29:42 -0400

    mode_perf: refactored send and receive functionality to optimize code refuse

--- src/mode_perf.c
+++ src/mode_perf.c
@@ -24,38 +24,119 @@ static struct threadobj thread;
 static struct args_obj *opts = NULL;
 
 /**
+ * @brief Set a socket configuration.
+ *
+ * @param[in,out] obj       A pointer to a socket object.
+ * @param[in]     timeoutms Socket timeout in milliseconds.
+ *
+ * @return Void.
+ */
+static void modeperf_conf(struct sockobj * const obj, const int32_t timeoutms)
+{
+    memcpy(obj->conf.ipaddr, opts->ipaddr, sizeof(obj->conf.ipaddr));
+    obj->conf.ipport        = opts->ipport;
+    obj->conf.timeoutms     = timeoutms;
+    obj->conf.datalimitbyte = opts->datalimitbyte;
+    obj->conf.ratelimitbps  = opts->ratelimitbps;
+    obj->conf.timelimitusec = opts->timelimitusec;
+    obj->conf.family        = opts->family;
+    obj->conf.type          = opts->type;
+    obj->conf.model         = opts->arch;;
+}
+
+/**
+ * @brief Call a socket's receive or send function.
+ *
+ * @param[in]     call   A pointer to a socket object's receive or send
+ *                       function.
+ * @param[in,out] stats  Function-specific socket statistics.
+ * @param[in,out] obj    A pointer to a socket object.
+ * @param[in]     buf    A pointer to a buffer.
+ * @param[in]     buflen The maximum size of a buffer in bytes.
+ * @param[in]     tsus   The current Unix time in microseconds.
+ *
+ * @return The number of bytes returned by a socket's receive or send function.
+ */
+static int32_t modeperf_call(int32_t (*call)(struct sockobj * const obj,
+                                          void * const buf,
+                                          const uint32_t len),
+                             struct sockobj_flowstats *stats,
+                             struct sockobj * const sock,
+                             void * const buf,
+                             const uint32_t buflen,
+                             const uint64_t tsus)
+{
+    int32_t retval = 0;
+    uint64_t len = 0;
+
+    if (opts->datalimitbyte > 0)
+    {
+        if (stats->totalbytes < opts->datalimitbyte)
+        {
+            len = opts->datalimitbyte - stats->totalbytes;
+
+            if (len > buflen)
+            {
+                len = buflen;
+            }
+
+            len = tokenbucket_remove(&sock->tb, len * 8) / 8;
+        }
+        else
+        {
+            len = 0;
+        }
+    }
+    else if (opts->timelimitusec > 0)
+    {
+        len = tokenbucket_remove(&sock->tb, buflen * 8) / 8;
+    }
+
+    if (len > 0)
+    {
+        retval = call(sock, buf, len);
+    }
+    // @todo poll socket if buflen is zero?
+
+    if ((opts->timelimitusec > 0) &&
+        ((tsus - sock->info.startusec) >= opts->timelimitusec))
+    {
+        sock->ops.sock_close(sock);
+    }
+    else if ((opts->datalimitbyte > 0) &&
+             (stats->totalbytes >= opts->datalimitbyte))
+    {
+        sock->ops.sock_close(sock);
+    }
+
+    tokenbucket_return(&sock->tb, retval < 0 ?
+                                  0 :
+                                  len - (uint32_t)retval);
+
+    if (retval < 0)
+    {
+        sock->ops.sock_close(sock);
+    }
+
+    return retval;
+}
+
+/**
  * @see See header file for interface comments.
  */
 static void *modeperf_thread(void * arg)
 {
-    bool exit = false;
+    bool exit = true;
     struct threadobj *thread = (struct threadobj *)arg;
     struct sockobj client, server, socket;
     struct formobj form;
-    struct tokenbucket tb;
     char *recvbuf = NULL, *sendbuf = NULL;
-    int32_t count = 0, timeoutms = 500;
+    int32_t count = 0;
     int32_t formbytes = 0, recvbytes = 0, sendbytes = 0;
 
     //??
     uint64_t activetimeus = 0;
-    uint64_t timeusec = 0;
-    uint64_t buflen = 0;
-
-    memcpy(client.conf.ipaddr, opts->ipaddr, sizeof(client.conf.ipaddr));
-    client.conf.ipport = opts->ipport;
-    client.conf.timeoutms = 0;
-    client.conf.timelimitusec = opts->timelimitusec;
-    client.conf.datalimitbyte = opts->datalimitbyte;
-    client.conf.family = opts->family;
-    client.conf.type = opts->type;
-    client.conf.model = SOCKOBJ_MODEL_CLIENT;
-    memcpy(server.conf.ipaddr, opts->ipaddr, sizeof(server.conf.ipaddr));
-    server.conf.ipport = opts->ipport;
-    server.conf.timeoutms = timeoutms;
-    server.conf.family = opts->family;
-    server.conf.type = opts->type;
-    server.conf.model = SOCKOBJ_MODEL_SERVER;
+    uint64_t tsus = 0;
 
     if (thread == NULL)
     {
@@ -65,10 +146,16 @@ static void *modeperf_thread(void * arg)
     }
     else if ((recvbuf = malloc(opts->buflen)) == NULL)
     {
-
+        logger_printf(LOGGER_LEVEL_ERROR,
+                      "%s: receive buffer allocation failed\n",
+                      __FUNCTION__);
     }
     else if ((sendbuf = malloc(opts->buflen)) == NULL)
     {
+        logger_printf(LOGGER_LEVEL_ERROR,
+                      "%s: send buffer allocation failed\n",
+                      __FUNCTION__);
+
         free(recvbuf);
         recvbuf = NULL;
     }
@@ -84,14 +171,14 @@ static void *modeperf_thread(void * arg)
 
         if (opts->arch == SOCKOBJ_MODEL_CLIENT)
         {
+            modeperf_conf(&client, 0);
             exit = !sockmod_init(&client);
         }
         else
         {
+            modeperf_conf(&server, 500);
             exit = !sockmod_init(&server);
         }
-
-        tokenbucket_init(&tb, opts->ratelimitbps);//(opts->ratelimitbps - 1) / 8 + 1); // Round up (x + (y-1)) / y or (x-1)/y + 1
 
         while ((exit == false) && (threadobj_isrunning(thread) == true))
         {
@@ -127,27 +214,32 @@ static void *modeperf_thread(void * arg)
                     }
                     else
                     {
-                        form.sock = &server;
-                        formbytes = formobj_idle(&form);
-                        output_if_std_send(form.dstbuf, formbytes);
-                        formbytes = utilstring_concat(form.dstbuf,
-                                                      form.dstlen,
-                                                      "%c",
-                                                      '\r');
-                        output_if_std_send(form.dstbuf, formbytes);
+                        if (server.event.revents & FIONOBJ_REVENT_ERROR)
+                        {
+                            // @todo Wait for any sockets to exit?
+                            exit = true;
+                        }
+                        else
+                        {
+                            form.sock = &server;
+                            formbytes = formobj_idle(&form);
+                            output_if_std_send(form.dstbuf, formbytes);
+                            formbytes = utilstring_concat(form.dstbuf,
+                                                          form.dstlen,
+                                                          "%c",
+                                                          '\r');
+                            output_if_std_send(form.dstbuf, formbytes);
+                        }
                     }
-
-                    // @todo Exit if server socket has fatal error.
                 }
             }
             else
             {
-                timeusec = utildate_gettstime(DATE_CLOCK_MONOTONIC,
-                                              UNIT_TIME_USEC);
+                tsus = utildate_gettstime(DATE_CLOCK_MONOTONIC, UNIT_TIME_USEC);
 
                 if (activetimeus == 0)
                 {
-                    activetimeus = timeusec;
+                    activetimeus = tsus;
                 }
 
                 if (opts->arch == SOCKOBJ_MODEL_CLIENT)
@@ -157,47 +249,17 @@ static void *modeperf_thread(void * arg)
                         client.ops.sock_connect(&client);
                     }
 
-                    if (opts->datalimitbyte > 0)
-                    {
-                        if (client.info.send.totalbytes < opts->datalimitbyte)
-                        {
-                            buflen = opts->datalimitbyte - client.info.send.totalbytes;
+                    sendbytes = modeperf_call(client.ops.sock_send,
+                                              &client.info.send,
+                                              &client,
+                                              sendbuf,
+                                              opts->buflen,
+                                              tsus);
 
-                            if (buflen > opts->buflen)
-                            {
-                                buflen = opts->buflen;
-                            }
-
-                            buflen = tokenbucket_remove(&tb, buflen * 8) / 8;
-                        }
-                        else
-                        {
-                            buflen = 0;
-                        }
-                    }
-                    else if (opts->timelimitusec > 0)
-                    {
-                        buflen = tokenbucket_remove(&tb, opts->buflen * 8) / 8;
-                    }
-
-                    // @todo use rate limiting input interface to send data.
-                    if (buflen > 0)
-                    {
-                        sendbytes = client.ops.sock_send(&client,
-                                                         sendbuf,
-                                                         buflen);
-                    }
-                    else
-                    {
-                        sendbytes = 0;
-                    }
-
-                    if ((opts->timelimitusec > 0) &&
-                        ((timeusec - client.info.startusec) >= opts->timelimitusec))
+                    if (client.state & SOCKOBJ_STATE_CLOSE)
                     {
                         formbytes = form.ops.form_foot(&form);
                         output_if_std_send(form.dstbuf, formbytes);
-                        client.ops.sock_close(&client);
                         exit = true;
 
                         logger_printf(LOGGER_LEVEL_INFO,
@@ -211,26 +273,6 @@ static void *modeperf_thread(void * arg)
                                       client.info.send.maxbuflen,
                                       client.info.send.minbuflen);
                     }
-                    else if ((opts->datalimitbyte > 0) &&
-                             (client.info.send.totalbytes >= opts->datalimitbyte))
-                    {
-                        formbytes = form.ops.form_foot(&form);
-                        output_if_std_send(form.dstbuf, formbytes);
-                        client.ops.sock_close(&client);
-                        exit = true;
-                    }
-
-                    tokenbucket_return(&tb, sendbytes < 0 ?
-                                            0 :
-                                            buflen - (uint32_t)sendbytes);
-
-                    if (sendbytes < 0)
-                    {
-                        formbytes = form.ops.form_foot(&form);
-                        output_if_std_send(form.dstbuf, formbytes);
-                        client.ops.sock_close(&client);
-                        exit = true;
-                    }
                     else
                     {
                         formbytes = form.ops.form_body(&form);
@@ -239,12 +281,13 @@ static void *modeperf_thread(void * arg)
                         // Prevent thread spin when no bytes are available.
                         if (sendbytes == 0)
                         {
-                            if (tb.rate > 0)
+                            if (client.tb.rate > 0)
                             {
                                 // @todo Replace with semaphore.
-                                usleep((uint32_t)tokenbucket_delay(&tb, opts->buflen * 8));
+                                usleep((uint32_t)tokenbucket_delay(&client.tb,
+                                                                   opts->buflen * 8));
                             }
-                            else if (timeusec - activetimeus > 2000)
+                            else if (tsus - activetimeus > 2000)
                             {
                                 client.event.timeoutms = 2;
                                 client.event.pevents = FIONOBJ_PEVENT_OUT;
@@ -254,36 +297,35 @@ static void *modeperf_thread(void * arg)
                         {
                             client.event.timeoutms = 0;
                             client.event.pevents = FIONOBJ_PEVENT_IN;
-                            activetimeus = timeusec;
+                            activetimeus = tsus;
                         }
                     }
                 }
                 else
                 {
-                    buflen = tokenbucket_remove(&tb, opts->buflen * 8) / 8;
+                    recvbytes = modeperf_call(socket.ops.sock_recv,
+                                              &socket.info.recv,
+                                              &socket,
+                                              recvbuf,
+                                              opts->buflen,
+                                              tsus);
 
-                    if (buflen > 0)
-                    {
-                        recvbytes = socket.ops.sock_recv(&socket,
-                                                         recvbuf,
-                                                         buflen);
-                    }
-                    else
-                    {
-                        recvbytes = 0;
-                    }
-
-                    tokenbucket_return(&tb, recvbytes < 0 ?
-                                            0 :
-                                            buflen - (uint32_t)recvbytes);
-
-                    if (recvbytes < 0)
+                    if (socket.state & SOCKOBJ_STATE_CLOSE)
                     {
                         formbytes = form.ops.form_foot(&form);
                         output_if_std_send(form.dstbuf, formbytes);
-
-                        socket.ops.sock_close(&socket);
                         count--;
+
+                        logger_printf(LOGGER_LEVEL_INFO,
+                                      "%s: passed %" PRIu64
+                                      ", failed %" PRIu64
+                                      ", avg:%u max:%u min:%u\n",
+                                      __FUNCTION__,
+                                      socket.info.recv.passedcalls,
+                                      socket.info.recv.failedcalls,
+                                      socket.info.recv.avgbuflen,
+                                      socket.info.recv.maxbuflen,
+                                      socket.info.recv.minbuflen);
                     }
                     else
                     {
@@ -293,12 +335,13 @@ static void *modeperf_thread(void * arg)
                         // Prevent thread spin when no bytes are available.
                         if (recvbytes == 0)
                         {
-                            if (tb.rate > 0)
+                            if (socket.tb.rate > 0)
                             {
                                 // @todo Replace with semaphore.
-                                usleep((uint32_t)tokenbucket_delay(&tb, opts->buflen * 8));
+                                usleep((uint32_t)tokenbucket_delay(&socket.tb,
+                                                                   opts->buflen * 8));
                             }
-                            else if (timeusec - activetimeus > 2000)
+                            else if (tsus - activetimeus > 2000)
                             {
                                 socket.event.timeoutms = 2;
                             }
@@ -306,7 +349,7 @@ static void *modeperf_thread(void * arg)
                         else
                         {
                             socket.event.timeoutms = 0;
-                            activetimeus = timeusec;
+                            activetimeus = tsus;
                         }
                     }
                 }
