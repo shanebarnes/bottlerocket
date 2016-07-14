1c9869648fe8b2548460912d6dcd39125ffd28c8
shanebarnes <shanebarnes@users.noreply.github.com>
2016-07-13 22:11:31 -0400

    util_mem: initial commit

---
+++ inc/util_mem.h
@@ -0,0 +1,23 @@
+/**
+ * @file      util_mem.h
+ * @brief     Memory allocation utility interface.
+ * @author    Shane Barnes
+ * @date      13 Jul 2016
+ * @copyright Copyright 2016 Shane Barnes. All rights reserved.
+ *            This project is released under the MIT license.
+ */
+
+#ifndef _UTIL_MEM_H_
+#define _UTIL_MEM_H_
+
+#include <stdlib.h>
+
+#define UTILMEM_FREE(ptr) (free(ptr))
+
+#if defined(__cpluscplus)
+    #define UTILMEM_MALLOC(type, count) ((type*)malloc(sizeof(type) * count))
+#else
+    #define UTILMEM_MALLOC(type, count) (malloc(sizeof(type) * count))
+#endif
+
+#endif // _UTIL_MEM_H_
