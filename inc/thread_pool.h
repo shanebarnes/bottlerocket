80e33bd34e1df39949e7d3a9a82fdea95e4d5c1e
shanebarnes <shanebarnes@users.noreply.github.com>
2016-09-14 21:38:55 -0400

    thread_pool: initial commit

---
+++ inc/thread_pool.h
@@ -0,0 +1,73 @@
+/**
+ * @file      thread_pool.h
+ * @brief     Thread pool interface.
+ * @author    Shane Barnes
+ * @date      14 Sep 2016
+ * @copyright Copyright 2016 Shane Barnes. All rights reserved.
+ *            This project is released under the MIT license.
+ */
+
+#ifndef _THREAD_POOL_H_
+#define _THREAD_POOL_H_
+
+#include "system_types.h"
+
+struct threadpool_priv;
+
+struct threadpool
+{
+    struct threadpool_priv *priv;
+};
+
+/**
+ * @brief Create a thread pool.
+ *
+ * @param[in,out] pool A pointer to a thread pool to create.
+ * @param[in]     size The number of worker threads to create..
+ *
+ * @return True if a thread pool was created.
+ */
+bool threadpool_create(struct threadpool * pool, const uint32_t size);
+
+/**
+ * @brief Destroy a thread pool.
+ *
+ * @param[in,out] pool A pointer to a thread pool to destroy.
+ *
+ * @return True if a thread pool was destroyed.
+ */
+bool threadpool_destroy(struct threadpool * const pool);
+
+/**
+ * @brief Start a thread pool.
+ *
+ * @param[in,out] pool A pointer to a thread pool to start.
+ *
+ * @return True if a thread pool was started.
+ */
+bool threadpool_start(struct threadpool * const pool);
+
+/**
+ * @brief Stop a thread pool.
+ *
+ * @param[in,out] pool A pointer to a thread pool to stop.
+ *
+ * @return True if a thread pool was stopped.
+ */
+bool threadpool_stop(struct threadpool * const pool);
+
+/**
+ * @brief Execute a task (i.e., a function with arguments) concurrently in a
+ *        thread pool worker thread.
+ *
+ * @param[in,out] pool A pointer to a thread pool.
+ * @param[in]     func A pointer to a task function.
+ * @param[in]     arg  A pointer to a task function argument.
+ *
+ * @return True if a task was executed in a thread pool worker thread.
+ */
+bool threadpool_execute(struct threadpool * const pool,
+                        void * const func,
+                        void * const arg);
+
+#endif // _THREAD_POOL_H_
---
+++ src/thread_pool.c
@@ -0,0 +1,296 @@
+/**
+ * @file      thread_pool.c
+ * @brief     Thread pool implementation.
+ * @author    Shane Barnes
+ * @date      14 Sep 2016
+ * @copyright Copyright 2016 Shane Barnes. All rights reserved.
+ *            This project is released under the MIT license.
+ */
+
+#include "logger.h"
+#include "cv_obj.h"
+#include "mutex_obj.h"
+#include "thread_obj.h"
+#include "thread_pool.h"
+#include "util_debug.h"
+#include "util_mem.h"
+#include "vector.h"
+
+#include <errno.h>
+#include <string.h>
+
+struct threadpool_priv
+{
+    struct cvobj    cv;
+    struct mutexobj mtx;
+    struct vector   threads;
+    struct vector   tasks;
+    bool            shutdown;
+};
+
+struct threadpool_task
+{
+    void (*func)();
+    void *arg;
+};
+
+/**
+ * @brief Thread pool task thread that waits to be signaled when a new task is
+ *        available to be executed.
+ *
+ * @param[in,out] arg A pointer to a thread pool.
+ */
+static void *threadpool_thread(void *arg)
+{
+    struct threadpool *pool = (struct threadpool*)arg;
+    bool shutdown = false;
+    struct threadpool_task task, *temp = NULL;
+
+    if (UTILDEBUG_VERIFY((pool != NULL) && (pool->priv != NULL)) == true)
+    {
+        while (shutdown == false)
+        {
+            task.func = NULL;
+            mutexobj_lock(&pool->priv->mtx);
+            cvobj_wait(&pool->priv->cv, &pool->priv->mtx);
+
+            shutdown = pool->priv->shutdown;
+            if (shutdown == false)
+            {
+                temp = vector_gettail(&pool->priv->tasks);
+                if (temp != NULL)
+                {
+                    task.func = temp->func;
+                    task.arg = temp->arg;
+                    vector_deletetail(&pool->priv->tasks);
+                }
+            }
+            mutexobj_unlock(&pool->priv->mtx);
+
+            if (task.func != NULL)
+            {
+                (*task.func)(task.arg);
+            }
+        }
+    }
+
+    return NULL;
+}
+
+/**
+ * @see See header file for interface comments.
+ */
+bool threadpool_create(struct threadpool * pool, const uint32_t size)
+{
+    bool ret = false;
+    uint32_t i = 0;
+    struct threadobj *thread = NULL;
+
+    if (UTILDEBUG_VERIFY((pool != NULL) &&
+                         (pool->priv == NULL) &&
+                         (size > 0)) == true)
+    {
+        pool->priv = UTILMEM_CALLOC(struct threadpool_priv,
+                                    sizeof(struct threadpool_priv),
+                                    1);
+
+        if (pool->priv == NULL)
+        {
+            logger_printf(LOGGER_LEVEL_ERROR,
+                          "%s: failed to allocate private memory (%d)\n",
+                          __FUNCTION__,
+                          errno);
+        }
+        else if (vector_create(&pool->priv->threads,
+                               size,
+                               sizeof(struct threadobj)) == false)
+        {
+            threadpool_destroy(pool);
+        }
+        else if (vector_create(&pool->priv->tasks,
+                               0,
+                               sizeof(struct threadpool_task)) == false)
+        {
+            threadpool_destroy(pool);
+        }
+        else if (cvobj_create(&pool->priv->cv) == false)
+        {
+            threadpool_destroy(pool);
+        }
+        else if (mutexobj_create(&pool->priv->mtx) == false)
+        {
+            threadpool_destroy(pool);
+        }
+        else
+        {
+            for (i = 0; i < vector_getsize(&pool->priv->threads); i++)
+            {
+                thread = vector_getval(&pool->priv->threads, i);
+                memset(thread, 0, sizeof(*thread));
+
+                if (threadobj_create(thread) == false)
+                {
+                    logger_printf(LOGGER_LEVEL_ERROR,
+                                  "%s: failed to create thread #%u\n",
+                                  __FUNCTION__,
+                                  i);
+                }
+                else
+                {
+                    thread->function = threadpool_thread;
+                    thread->argument = pool;
+                }
+            }
+
+            if (i != vector_getsize(&pool->priv->threads))
+            {
+                logger_printf(LOGGER_LEVEL_ERROR,
+                              "%s: failed to create thread pool\n",
+                              __FUNCTION__);
+                threadpool_destroy(pool);
+            }
+            else
+            {
+                pool->priv->shutdown = true;
+                ret = true;
+            }
+        }
+    }
+
+    return ret;
+}
+
+/**
+ * @see See header file for interface comments.
+ */
+bool threadpool_destroy(struct threadpool * const pool)
+{
+    bool ret = false;
+    uint32_t i = 0;
+    struct threadobj *thread = NULL;
+
+    if (UTILDEBUG_VERIFY((pool != NULL) && (pool->priv != NULL)) == true)
+    {
+        threadpool_stop(pool);
+        mutexobj_destroy(&pool->priv->mtx);
+        cvobj_destroy(&pool->priv->cv);
+        vector_destroy(&pool->priv->tasks);
+
+        for (i = 0; i < vector_getsize(&pool->priv->threads); i++)
+        {
+            thread = vector_getval(&pool->priv->threads, i);
+
+            if (threadobj_destroy(thread) == false)
+            {
+                logger_printf(LOGGER_LEVEL_ERROR,
+                              "%s: failed to destroy thread #%u\n",
+                              __FUNCTION__,
+                              i);
+            }
+        }
+
+        vector_destroy(&pool->priv->threads);
+        UTILMEM_FREE(pool->priv);
+        pool->priv = NULL;
+        ret = true;
+    }
+
+    return ret;
+}
+
+/**
+ * @see See header file for interface comments.
+ */
+bool threadpool_start(struct threadpool * const pool)
+{
+    bool ret = false;
+    uint32_t i = 0;
+    struct threadobj *thread = NULL;
+
+    if (UTILDEBUG_VERIFY((pool != NULL) && (pool->priv != NULL)) == true)
+    {
+        mutexobj_lock(&pool->priv->mtx);
+        pool->priv->shutdown = false;
+        mutexobj_unlock(&pool->priv->mtx);
+
+        for (i = 0; i < vector_getsize(&pool->priv->threads); i++)
+        {
+            thread = vector_getval(&pool->priv->threads, i);
+            if (threadobj_start(thread) == false)
+            {
+                logger_printf(LOGGER_LEVEL_ERROR,
+                              "%s: failed to start thread #%u\n",
+                              __FUNCTION__,
+                              i);
+            }
+            else
+            {
+                ret = true;
+            }
+        }
+    }
+
+    return ret;
+}
+
+/**
+ * @see See header file for interface comments.
+ */
+bool threadpool_stop(struct threadpool * const pool)
+{
+    bool ret = false;
+    uint32_t i = 0;
+    struct threadobj *thread = NULL;
+
+    if (UTILDEBUG_VERIFY((pool != NULL) && (pool->priv != NULL)) == true)
+    {
+        mutexobj_lock(&pool->priv->mtx);
+        pool->priv->shutdown = true;
+        mutexobj_unlock(&pool->priv->mtx);
+        cvobj_signalall(&pool->priv->cv);
+
+        for (i = 0; i < vector_getsize(&pool->priv->threads); i++)
+        {
+            thread = vector_getval(&pool->priv->threads, i);
+
+            if (threadobj_stop(thread) == false)
+            {
+                logger_printf(LOGGER_LEVEL_ERROR,
+                              "%s: failed to stop thread #%u\n",
+                              __FUNCTION__,
+                              i);
+            }
+            else
+            {
+                ret = true;
+            }
+        }
+    }
+
+    return ret;
+}
+
+/**
+ * @see See header file for interface comments.
+ */
+bool threadpool_execute(struct threadpool * const pool,
+                        void * const func,
+                        void * const arg)
+{
+    bool ret = false;
+    struct threadpool_task task = {.func = func, .arg = arg};
+
+    if (UTILDEBUG_VERIFY((pool != NULL) &&
+                         (pool->priv != NULL) &&
+                         (func != NULL)) == true)
+    {
+        mutexobj_lock(&pool->priv->mtx);
+        // @todo Replace vector with circular queue.
+        vector_inserttail(&pool->priv->tasks, &task);
+        mutexobj_unlock(&pool->priv->mtx);
+
+        cvobj_signalone(&pool->priv->cv);
+    }
+
+    return ret;
+}
