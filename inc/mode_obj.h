3779c7092d7a126126311fb32ee363c739016170
shanebarnes <shanebarnes@users.noreply.github.com>
2016-11-05 23:01:29 -0400

    mode_obj: added create/destroy functions

--- inc/mode_obj.h
+++ inc/mode_obj.h
@@ -14,9 +14,30 @@
 #include "system_types.h"
 
 struct modeobj;
+struct modeobj_priv;
 
 struct modeobj_ops
 {
+    /**
+     * @brief Create a mode object.
+     *
+     * @param[in,out] mode A pointer to a mode object.
+     * @param[in]     args A pointer to an arguments object.
+     *
+     * @return True if a mode object was created.
+     */
+    bool (*mode_create)(struct modeobj * const mode,
+                        const struct args_obj * const args);
+
+    /**
+     * brief Destroy a mode object.
+     *
+     * @param[in,out] mode A pointer to a mode object.
+     *
+     * @return True if a mode object was destroyed.
+     */
+    bool (*mode_destroy)(struct modeobj * const mode);
+
     /**
      * @brief Initialize a mode of operation.
      *
@@ -29,28 +50,35 @@ struct modeobj_ops
     /**
      * @brief Start a mode of operation.
      *
+     * @param[in,out] mode A pointer to a mode object.
+     *
      * @return True if mode of operation was started.
      */
-    bool (*mode_start)(void);
+    bool (*mode_start)(struct modeobj * const mode);
 
     /**
      * @brief Stop a mode of operation.
      *
+     * @param[in,out] mode A pointer to a mode object.
+     *
      * @return True if mode of operation was stopped.
      */
-    bool (*mode_stop)(void);
+    bool (*mode_stop)(struct modeobj * const mode);
 
     /**
      * @brief Cancel a mode of operation (i.e., unblock the mode_start).
      *
+     * @param[in,out] mode A pointer to a mode object.
+     *
      * @return True if mode of operation was canceled.
      */
-    bool (*mode_cancel)(void);
+    bool (*mode_cancel)(struct modeobj * const mode);
 };
 
 struct modeobj
 {
-    struct modeobj_ops ops;
+    struct modeobj_ops   ops;
+    struct modeobj_priv *priv;
 };
 
 #endif // _MODE_OBJ_H_
